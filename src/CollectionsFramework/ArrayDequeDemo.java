20de099d113e68404fc1197abecc5ec7beb3d0fc
besteffects <besteffects@gmail.com>
2019-12-12 01:53:57 -0500

    ArrayDeque

---
+++ src/CollectionsFramework/ArrayDequeDemo.java
@@ -0,0 +1,26 @@
+package CollectionsFramework;
+
+import java.util.ArrayDeque;
+
+class ArrayDequeDemo {
+    // It creates a dynamic array and has no capacity restrictions
+
+    public static void main(String[] args) {
+        //Create an array deque.
+        ArrayDeque<String> adq = new ArrayDeque<String>();
+
+        //Use an ArrayDeque like a stack.
+        adq.push("A");
+        adq.push("B");
+        adq.push("D");
+        adq.push("E");
+        adq.push("F");
+
+        System.out.println("Popping the stack: ");
+        while (adq.peek()!=null){
+            System.out.print(adq.pop() + " "); //Returns elements from the head of the queue
+        }
+        System.out.println();
+
+    }
+}
