51be2c91421a5c811880b925a70bb864bda0cd52
besteffects <besteffects@gmail.com>
2019-12-14 00:57:58 -0500

    Use thenComparing() to sort by last, then first name

--- src/comparator/sortingTreeMap/thenComparing/CompLastNames.java
+++ src/comparator/sortingTreeMap/thenComparing/CompLastNames.java
@@ -2,6 +2,9 @@ package comparator.sortingTreeMap.thenComparing;
 //Use thenComparing() to sort by last, then first name
 
 import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 //A comparator that compares last names.
 public class CompLastNames implements Comparator<String> {
@@ -13,5 +16,47 @@ public class CompLastNames implements Comparator<String> {
         j = bStr.lastIndexOf(' ');
 
         return aStr.substring(i).compareToIgnoreCase(bStr.substring(j));
+    }
+}
+
+// Sort the entire name when last names are equal
+class CompThenbyFirstName implements Comparator<String> {
+    public int compare(String aStr, String bStr) {
+        int i, j;
+        return aStr.compareToIgnoreCase(bStr);
+    }
+}
+
+class TreeMapDemo2A {
+    public static void main(String[] args) {
+        // Use thenComparing() o create a comparator that compares last names,
+        // then compares entire name when last names match.
+        CompLastNames compLN = new CompLastNames();
+        Comparator<String> compLastThenFirst = compLN.thenComparing(new CompThenbyFirstName());
+
+        //Create a tree map
+        TreeMap<String, Double> tm = new TreeMap<String, Double>(compLastThenFirst);
+
+        //Put elements to the map
+        tm.put("John Doe", 3434.34);
+        tm.put("Jane Baker", 1378.00);
+        tm.put("Tod Hall", 99.22);
+        tm.put("Ralph Smith", -19.08);
+
+        // Get a set of the entries
+        Set<Map.Entry<String, Double>> set = tm.entrySet();
+
+        //Display the elements
+        for (Map.Entry<String, Double> me : set) {
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+        }
+        System.out.println();
+
+        //Deposit 1000 into John Doe's account.
+        double balance = tm.get("John Doe");
+        tm.put("John Doe", balance + 1000);
+
+        System.out.println("John Doe's new balance: " + tm.get("John Doe"));
     }
 }
